3b4cc69f420852415a30cff93b36cf972c9c4cc4
Harish <hapk.inc@gmail.com>
2021-08-17 01:41:53 +0530

    SHA1 ID's changed both in prod and dev

--- android/app/src/main/kotlin/com/hapk/paricon/MainActivity.kt
+++ android/app/src/main/kotlin/com/hapk/paricon/MainActivity.kt
@@ -1,20 +1,6 @@
 package com.hapk.paricon
 
-/* import android.os.Bundle
-import android.os.PersistableBundle
-import com.google.firebase.FirebaseApp*/
-/*import com.google.firebase.appcheck.FirebaseAppCheck
-import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory*/
+
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() /*{
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-
-        FirebaseApp.initializeApp(this)
-        val firebaseAppCheck = FirebaseAppCheck.getInstance()
-        firebaseAppCheck.installAppCheckProviderFactory(
-                DebugAppCheckProviderFactory.getInstance()
-        )
-    }
-}*/
+class MainActivity: FlutterActivity()

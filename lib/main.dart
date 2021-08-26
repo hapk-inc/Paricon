6df57708a6f19a9d1c39390c70b124aeb0d44ed7
Harish <hapk.inc@gmail.com>
2021-08-26 22:30:14 +0530

    MobileAds instance calling

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:in_app_update/in_app_update.dart';
 
 import 'screens/appUpdate.dart';
@@ -16,6 +17,7 @@ import 'screens/welcome.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(ProviderScope(child: MyApp()));
 }
 
